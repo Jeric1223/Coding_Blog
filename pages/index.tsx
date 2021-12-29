9e93053f5d0a100ccfb33f645d93b8f7f43340c8
JaeHyun <soehd0889@naver.com>
2021-12-29 16:34:58 +0900

    Setting

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return <h1>TEST</h1>;
+};
+
+export default Home;
