7257a96f4fe0b08b3a331a882c2359c73dbc05c3
JaeHyun <soehd0889@naver.com>
2021-12-30 11:32:38 +0900

    [ADD] Header 컴포넌트 추가

---
+++ components/common/header/index.tsx
@@ -0,0 +1,13 @@
+import { FC, useEffect, useState } from "react";
+
+interface Props {}
+
+const Header: FC<Props> = () => {
+  return (
+    <>
+      <h1>test</h1>
+    </>
+  );
+};
+
+export default Header;
