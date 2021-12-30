1e46aa0aed903ef7632b28fa2ed5f1e3214423c9
JaeHyun <soehd0889@naver.com>
2021-12-30 11:32:56 +0900

    [ADD] 스타일 임포트

--- components/page/mainPage/index.tsx
+++ components/page/mainPage/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import * as S from "./styles";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  );
 };
 
 export default index;
