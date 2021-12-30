3630e7b01ab0aa6dd57ff5507cce930204ac8766
JaeHyun <soehd0889@naver.com>
2021-12-30 11:33:20 +0900

    [ROUTING]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import Main from "../components/page/mainPage/index";
+/* import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
+import Link from "next/link"; */
 
 const Home: NextPage = () => {
-  return <h1>TEST</h1>;
+  return <Main />;
 };
 
 export default Home;
