4434aba79077248fda3a981c7bc5f21f342b1ba3
JaeHyun <soehd0889@naver.com>
2022-01-03 16:02:36 +0900

    [ADD] web 아이콘 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
 import Main from "../components/page/mainPage/index";
-import Jumbotron from "../components/common/jumbotron/index";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Jumbotron />
+      <Head>
+        <title>나의 코딩일지</title>
+        <link rel="icon" href="/headIcon.png" />
+      </Head>
       <Main />
     </>
   );
