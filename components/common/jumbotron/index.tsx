3534a323d528f4c74a2f96db1e8a76ec684da3f4
JaeHyun <soehd0889@naver.com>
2022-01-03 14:08:55 +0900

    [ADD] 배너 퍼블리싱

--- components/common/jumbotron/index.tsx
+++ components/common/jumbotron/index.tsx
@@ -1,13 +1,23 @@
 import { FC, useEffect, useState } from "react";
+import * as S from "./style";
+import Image from "next/image";
 
 interface Props {}
 
-const Header: FC<Props> = () => {
+const Jumbotron: FC<Props> = () => {
   return (
     <>
-      <div>Header</div>
+      <S.JumbotronNav>
+        <S.JumbotronTitle>
+          <h1>나의 코딩 일지</h1>
+          <p>
+            이 사이트는 코딩을 배우면서 내가 무엇을 하였는지 일지를 쓰는
+            사이트입니다.
+          </p>
+        </S.JumbotronTitle>
+      </S.JumbotronNav>
     </>
   );
 };
 
-export default Header;
+export default Jumbotron;
---
+++ components/common/jumbotron/style.ts
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+export const JumbotronNav = styled.nav`
+  width: 100%;
+  height: 295px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("/jumbotron.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export const JumbotronTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0 15%;
+  & > * {
+    color: white;
+    font-family: "Noto Serif KR", serif;
+  }
+  & h1 {
+    font-size: 3.5rem;
+    font-weight: 500;
+  }
+  & p {
+    font-size: 1.25rem;
+  }
+`;
