fdc0f92e9301f343902cb3a6b7eccc2d12fc3dc7
JaeHyun <soehd0889@naver.com>
2022-01-07 11:26:35 +0900

    [ADD] 컴포넌트 추가

--- components/page/mainPage/index.tsx
+++ components/page/mainPage/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import * as S from "./styles";
+import Jumbotron from "../../common/jumbotron";
+import DiaryList from "../../common/diaryList";
 
 const index = () => {
   return (
     <>
-      <div>
-        
-      </div>
+
+      <Jumbotron />
+      <DiaryList />
     </>
   );
 };
