f10b84965fefaa6600536678ca1fb378854f7aca
JaeHyun <soehd0889@naver.com>
2022-01-12 16:39:06 +0900

    [ADD] list 퍼블리싱 완료

--- components/common/diaryList/ListItem.tsx
+++ components/common/diaryList/ListItem.tsx
@@ -6,13 +6,13 @@ interface Props {}
 const ListItem: FC<Props> = () => {
   return (
     <S.ListItem>
-      <span>FrontEnd</span>
+      <span className="field" >FRONTEND</span>
       <S.ListItemContent>
-        <div>
+        <div className="listItemTitleDiv">
           <h1>REACT 버전 18, 놀라운 기능!!</h1>
           <p>안녕하세요 오늘은 React 버전 18에 대해서 알아보겠습니다.</p>
         </div>
-        <div>
+        <div className="listItemDateDiv">
           <span>Jeric Kim</span>
           <span>2022-12-23</span>
         </div>
