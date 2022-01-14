d3a9ec39fb3c37e89365127eacd48b948478d810
JaeHyun <soehd0889@naver.com>
2022-01-14 14:04:17 +0900

    [ADD] toast ui editor

---
+++ template/PostEditor.tsx
@@ -0,0 +1,26 @@
+import Prism from "prismjs";
+// 여기 css를 수정해서 코드 하이라이팅 커스텀 가능
+import "prismjs/themes/prism.css";
+
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+
+import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
+
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
+
+export default function PostEditor(props: any) {
+  console.log(props);
+  return (
+    <Editor
+      ref={props.editorRef}
+      onChange={props.onChangeEvent}
+      previewStyle="vertical"
+      height="90vh"
+      plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
+    />
+  );
+}
