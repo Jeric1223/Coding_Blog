061810639e15a9af4f32ec9364835b3c7b6bee9b
Jeric <soehd0889@gmail.com>
2022-01-21 21:04:02 +0900

    [ADD] header title

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
 import * as S from "./style";
+import Link from "next/link";
 
 interface Props {}
 
 const index: FC<Props> = () => {
-  return <S.Header></S.Header>;
+  return (
+  <S.Header>
+    <Link href="/"> 
+      <h1 className="headerTitle">나의 코딩일지</h1>
+    </Link>
+  </S.Header>
+  )
 };
 
 export default index;
--- components/common/header/style.ts
+++ components/common/header/style.ts
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
 
 export const Header = styled.nav`
+  display: flex;
   width: 100%;
   height: 70px;
+  background: black;
 `;
+
+
