b1cd20a63e8ba934edb42f2b2afe790aee88c1cb
JaeHyun <soehd0889@naver.com>
2022-01-24 11:21:03 +0900

    [ADD] Header Title styled

--- components/common/header/style.ts
+++ components/common/header/style.ts
@@ -5,6 +5,11 @@ export const Header = styled.nav`
   width: 100%;
   height: 70px;
   background: black;
+  align-items: center;
+  h1 {
+    color: white;
+    font-family: "Poppins", "Noto Sans KR", sans-serif;
+    cursor: pointer;
+    margin-left: 20px;
+  }
 `;
-
-
