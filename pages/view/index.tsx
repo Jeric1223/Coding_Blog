4cc7ac57efc166054e02a0fddefaeba64b0b6261
JaeHyun <soehd0889@naver.com>
2022-01-25 15:14:59 +0900

    [ADD] viewer pages

---
+++ pages/view/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import DetailViewPage from "../../components/page/detailViewPage";
+
+
+const View: FC = () => {
+  return <DetailViewPage />;
+};
+
+export default View;
