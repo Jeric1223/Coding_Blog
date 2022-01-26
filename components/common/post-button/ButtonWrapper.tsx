40befc9230cc15e9ca7c828ce0790863d23abd18
Jeric <soehd0889@gmail.com>
2022-01-26 20:57:36 +0900

    [ADD] jotai local Storage

--- components/common/post-button/ButtonWrapper.tsx
+++ components/common/post-button/ButtonWrapper.tsx
@@ -1,17 +1,25 @@
 import React, { FC } from "react";
 import * as S from "./style";
+import { atom, useAtom } from 'jotai';
+import { atomWithStorage } from "jotai/utils"
 
 interface Props {
   editorMarkDown: string;
 }
 
+const darkModeAtom = atomWithStorage('isDone', false)
+
 const ButtonWrapper: FC<Props> = (props) => {
   const { editorMarkDown } = props;
+  const [darkMode, setDarkMode] = useAtom(darkModeAtom)
+
   return (
     <S.PostButtonWrapper>
       <button
         onClick={() => {
-          console.log(editorMarkDown);
+          console.log(editorMarkDown)
+          setDarkMode(!darkMode)
+          console.log(darkMode)
         }}
       >
         작성완료
