f00ca7824b2edeeb45a7b9c2f8bbc453e219e6fd
JaeHyun <soehd0889@naver.com>
2022-01-28 21:39:47 +0900

    [ADD] post page 제목과 부제목 useState 설정

--- components/page/postPage/index.tsx
+++ components/page/postPage/index.tsx
@@ -2,6 +2,7 @@ import React, { FC, useRef, useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Header from "../../common/header/index";
 import ButtonWrapper from "../../common/post-button/ButtonWrapper";
+import PostTitleWrapper from "../../common/post-title-wrapper/PostTitleWrapper";
 
 
 const PostEditor = dynamic(() => import("../../../template/PostEditor"), {
@@ -13,23 +14,37 @@ interface Props {}
 
 const PostPage: FC<Props> = () => {
   const [editorMarkDown, setEditorMarkDown] = useState("");
-
+  const [postTitleAndSubtitle, setPostTitleAndSubtitle] = useState(
+    {
+      title: "",
+      subtitle: ""
+    }
+  )
 
   useEffect(() => {
-    console.log(editorMarkDown);
-  }, [editorMarkDown]);
+    console.log(postTitleAndSubtitle);
+  }, [postTitleAndSubtitle]);
 
   const editorRef: any = useRef();
 
-  const onChangeEvent = () => {
+  const onChangeContent= () => {
     setEditorMarkDown(editorRef.current.getInstance().getMarkdown());
   };
+
+  const onChangeTitle = (e:any) => {
+    const { value, name } = e.target;
+    setPostTitleAndSubtitle({
+      ...postTitleAndSubtitle,
+      [name]: value
+    })
+  }
 
   return (
     <>
       <Header />
-      <PostEditor editorRef={editorRef} onChangeEvent={onChangeEvent} />
-      <ButtonWrapper editorMarkDown={editorMarkDown} />
+      <PostTitleWrapper onChangeTitle={onChangeTitle}  />
+      <PostEditor editorRef={editorRef} onChangeEvent={onChangeContent} />
+      <ButtonWrapper editorMarkDown={editorMarkDown} postTitleAndSubtitle={postTitleAndSubtitle} />
     </>
   );
 };
