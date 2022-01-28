fb98a76f5c76dce7e7ce48968c24c586df88b831
Jeric <soehd0889@gmail.com>
2022-01-28 14:19:02 +0900

    [ADD] 글 작성 완료 로직 변경

--- components/common/post-button/ButtonWrapper.tsx
+++ components/common/post-button/ButtonWrapper.tsx
@@ -1,26 +1,43 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import * as S from "./style";
 import { atom, useAtom } from 'jotai';
 import { atomWithStorage } from "jotai/utils"
 
 interface Props {
   editorMarkDown: string;
+  postTitleAndSubtitle: { title: string, subtitle: string }
 }
 
 const darkModeAtom = atomWithStorage('postItem', "[]")
 
 const ButtonWrapper: FC<Props> = (props) => {
-  const { editorMarkDown } = props;
+  const { editorMarkDown, postTitleAndSubtitle } = props;
+  const [postItem, setPostItem] = useAtom(darkModeAtom);
 
-  const [postItem, setPostItem] = useAtom(darkModeAtom)
+  const titleCheckFunc = () => {
+    const { title, subtitle } = postTitleAndSubtitle;
+    if(title.length <= 0 ) {
+      alert('제목을 입력해 주세요.')
+      return false
+    }
+    if(subtitle.length <= 0 ) {
+      alert('부제목을 입력해 주세요.')
+      return false
+    }
+    else {
+      return true
+    }
+  }
 
   return (
     <S.PostButtonWrapper>
       <button
         onClick={() => {
-          let newArray = JSON.parse(postItem)
-          newArray.push(editorMarkDown)
-          setPostItem(JSON.stringify(newArray))
+          if(titleCheckFunc()){
+            let newArray = JSON.parse(postItem)
+            newArray.push({editorMarkDown:editorMarkDown, title:postTitleAndSubtitle.title, subtitle:postTitleAndSubtitle.subtitle})
+            setPostItem(JSON.stringify(newArray))
+          }
         }}
       >
         작성완료
