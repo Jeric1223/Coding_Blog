9cfb9e48c3043c755e7d80287563c486f81cde58
JaeHyun <soehd0889@naver.com>
2022-01-28 21:40:27 +0900

    [ADD] post page title subtitle 컴포넌트 스타일 추가

---
+++ components/common/post-title-wrapper/style.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const MainWrapper = styled.div`
+    margin: 20px 0 10px 50px;
+    display: flex;
+    flex-direction: column;
+    input{
+
+        width: 80vw;
+        height: 60px;
+        font-family: "Poppins", "Noto Sans KR", sans-serif;
+        font-size: 24px;
+        padding-left: 20px;
+    }
+    input:nth-of-type(2){
+        font-size: 20px;
+    }
+`;
